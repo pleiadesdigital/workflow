a15b91a5911b0f29d40d8975434da9625b273e27
Ronaldo Ortiz <ronyortizop@yahoo.com>
2018-12-26 11:10:33 -0400

    vid 42 done BEFORE recycling code

--- single-professor.php
+++ single-professor.php
@@ -2,12 +2,12 @@
 
 <!-- PAGE BANNER -->
 <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
-
+	<?php $pageBannerImage = get_field('page_banner_background_image'); ?>
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo $pageBannerImage['sizes']['pageBanner']; ?>)"></div>
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title"><?php the_title(); ?></h1>
     <div class="page-banner__intro">
-      <p>CHANGE LATER</p>
+      <p><?php the_field('page_banner_subtitle'); ?></p>
     </div>
   </div>
 </div>
